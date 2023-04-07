ff6b4bb067b16152d50835c7fac40a1de6846e05
LyapunKate <91290812+LyapunKate@users.noreply.github.com>
2023-04-08 01:20:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 532569024 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    import math
+    n = len(x)
+    new_x = x - 887
+    sum = 0
+    for i in range(n):
+      sum += math.log(new_x[i])
+    a = sum/n
+    return a
